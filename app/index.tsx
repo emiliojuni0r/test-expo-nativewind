148b960a9fcf96c1818d2c72ed74b30508519978
emiliojuni0r <emiliohamsyahjr@gmail.com>
2025-04-25 11:17:01 +0700

    https://github.com/emiliojuni0r/test-expo-nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-600">
+      <Text className="text-5xl text-pink-500">welcome cihuuy</Text>
+    </View>
+  );
+}
